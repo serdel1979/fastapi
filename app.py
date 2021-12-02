086d7f07b99525eff8a5e620115d2f2672783a1f
unknown <sdlbsso@gmail.com>
2021-12-02 13:47:30 -0300

    crud con fastapi

--- app.py
+++ app.py
@@ -33,7 +33,6 @@ def get_posts():
 def save_post(post: Post):
     post.id = str(uuid())
     posts.append(post.dict())
-    print(posts)
     return posts[-1]
 
 @app.get('/posts/{post_id}')
@@ -42,3 +41,24 @@ def get_post(post_id:str):
         if p['id'] == post_id:
             return p
     raise HTTPException(status_code=404, detail = "No encontrado")
+
+@app.delete("/posts/{post_id}")
+def delete_post(post_id):
+    for index,post in enumerate(posts):
+       if post["id"] == post_id:
+           posts.pop(index)
+           return {"message": "La publicacion se eliminó"}
+    return "No encontrado"
+
+@app.put("/posts/{post_id}")
+def update_post(post_id: str, updatePost: Post):
+    for index, post in enumerate(posts):
+        if post["id"] == post_id:
+            posts[index]["title"]= updatePost.title
+            posts[index]["content"]= updatePost.content
+            posts[index]["author"]= updatePost.author
+            return {"message": "La publicacion se editó"}
+    raise HTTPException(status_code=404, detail = "No encontrado")
+        
+
+
