3f62a9ca16e9a1591077920d45dc9165238d5995
Sergio De Luca <sdlbsso@gmail.com>
2021-12-02 01:24:50 -0300

    REST api CRUD de publicaciones

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
-from fastapi import FastAPI
+from typing import Text, Optional
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from datetime import datetime
+from uuid import uuid4 as uuid
 
 app = FastAPI()
 
 posts = []
+
+#modelo de un Post 
+
+class Post(BaseModel):
+    id: Optional[str]
+    title: str
+    author: str
+    content: Text
+    created_at: datetime = datetime.now()
+    published_at: Optional[datetime]
+    published = bool = False
+
+
 
 @app.get('/')
 def read_root():
@@ -11,3 +28,17 @@ def read_root():
 @app.get('/posts')
 def get_posts():
     return posts
+
+@app.post('/posts')
+def save_post(post: Post):
+    post.id = str(uuid())
+    posts.append(post.dict())
+    print(posts)
+    return posts[-1]
+
+@app.get('/posts/{post_id}')
+def get_post(post_id:str):
+    for p in posts:
+        if p['id'] == post_id:
+            return p
+    raise HTTPException(status_code=404, detail = "No encontrado")
